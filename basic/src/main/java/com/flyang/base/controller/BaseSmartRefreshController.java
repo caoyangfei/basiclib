c90fb6a1858c9234a5b7adfe8439bff4af7f2a09
unknown <caoyangfei@supcon.com>
2019-10-24 17:27:10 +0800

    曹阳飞
    添加刷新的controller

---
+++ basic/src/main/java/com/flyang/base/controller/BaseSmartRefreshController.java
@@ -0,0 +1,11 @@
+package com.flyang.base.controller;
+
+/**
+ * @author yangfei.cao
+ * @ClassName basiclib
+ * @date 2019/10/24
+ * ------------- Description -------------
+ * 刷新的controller
+ */
+public class BaseSmartRefreshController {
+}
