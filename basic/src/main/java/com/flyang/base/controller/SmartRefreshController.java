58d230e03bfc02f887493d3ea7df81b218a08e22
unknown <caoyangfei@supcon.com>
2019-10-25 13:16:33 +0800

    曹阳飞
    修改版本配置

---
+++ basic/src/main/java/com/flyang/base/controller/SmartRefreshController.java
@@ -0,0 +1,19 @@
+package com.flyang.base.controller;
+
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+/**
+ * @author yangfei.cao
+ * @ClassName SmartRefreshController
+ * @date 2019/10/24
+ * ------------- Description -------------
+ * 刷新的controller
+ */
+public class SmartRefreshController extends BasePresenterController {
+//    @BindView("")
+    public SmartRefreshController(FragmentActivity activity, View rootView) {
+        super(activity, rootView);
+
+    }
+}
